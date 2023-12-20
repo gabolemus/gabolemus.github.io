aae3eb83cf820b9e18b3332866f1a8b3dfd5b128
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-12-19 18:07:13 -0600

    Add MainInfoCard component with styling

---
+++ src/components/molecules/MainInfoCard/MainInfoCard.tsx
@@ -0,0 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+import Card from "../Card/Card";
+import CardColumn from "../CardColumn/CardColumn";
+
+const MainInfoCard = () => {
+  return (
+    <Card title="Gabriel Lemus">
+      <CardColumn>
+        <a href="https://github.com/gabolemus" target="_blank" rel="noreferrer">
+          <img
+            src="https://avatars.githubusercontent.com/u/49124491?v=4"
+            alt="GitHub profile picture"
+            className="about-me__image"
+          />
+        </a>
+        <p className="about-me__description">Systems Engineer</p>
+        <div className="about-me__icons">
+          <a href="mailto:gabriel.lemusp@gmail.com">
+            <FontAwesomeIcon icon={icon({ name: "envelope" })} />
+          </a>
+          <a
+            href="https://github.com/gabolemus"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={icon({ name: "github", style: "brands" })} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gabriel-lemus-pacheco/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={icon({ name: "linkedin", style: "brands" })}
+            />
+          </a>
+        </div>
+      </CardColumn>
+    </Card>
+  );
+};
+
+export default MainInfoCard;
