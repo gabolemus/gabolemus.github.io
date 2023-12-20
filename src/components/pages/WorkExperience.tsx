3639d2fff5dd3a33bfed7ac091127498d0cbf9cf
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-12-19 18:08:16 -0600

    Add WorkExperience page component

---
+++ src/components/pages/WorkExperience.tsx
@@ -0,0 +1,12 @@
+import MainPage from "../templates/MainPage";
+
+/** Component for the page that displays my work experience */
+const WorkExperience = () => {
+  return (
+    <MainPage>
+      <h1>Work Experience</h1>
+    </MainPage>
+  );
+};
+
+export default WorkExperience;
