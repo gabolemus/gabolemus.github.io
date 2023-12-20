621e8b53f89d266f2c7bba90ad6dd6e20f9541b4
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-12-19 18:07:34 -0600

    Add SkillsRow component

---
+++ src/components/molecules/SkillsRow/SkillsRow.tsx
@@ -0,0 +1,48 @@
+import "./SkillsRow.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faServer } from "@fortawesome/free-solid-svg-icons";
+import { faDocker } from "@fortawesome/free-brands-svg-icons";
+import { faDev } from "@fortawesome/free-brands-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+
+/** Object that maps icon names to icon components */
+export const projectIcons = {
+  /** Web development icon */
+  globe: faGlobe,
+  /** Backend development icon */
+  server: faServer,
+  /** Docker icon */
+  docker: faDocker,
+  /** Dev frameworks icon */
+  frameworks: faDev,
+  /** Programming languages icon */
+  languages: faCode,
+};
+
+/** Props for SkillsRow component */
+export interface SkillsRowProps {
+  /** Icon to render */
+  iconName: keyof typeof projectIcons;
+  /** Title to render */
+  title: string;
+  /** Description to render */
+  description: string;
+}
+
+const SkillsRow = ({ iconName, title, description }: SkillsRowProps) => {
+  console.log(`Type of iconName: ${typeof iconName}`);
+  console.log(`iconName: ${iconName}`);
+
+  return (
+    <div className="skills__row">
+      <FontAwesomeIcon className="skills__icon" icon={projectIcons[iconName]} />
+      <div className="skills__info">
+        <h4 className="skills__title">{title}</h4>
+        <p className="skills__description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SkillsRow;
