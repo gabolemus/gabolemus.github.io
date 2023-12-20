8b2a1b23cbd58d6c471d819fb2c252820cc3f2b8
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-12-19 18:06:00 -0600

    Add page routes to utils/pages.tsx

---
+++ src/utils/pages.tsx
@@ -0,0 +1,30 @@
+// This file contains the app's page routes.
+
+import Home from "../components/pages/Home";
+import WorkExperience from "../components/pages/WorkExperience";
+
+/** Interface for the pages to be shown in the navbar */
+export interface Page {
+  /** Path of the page */
+  path: string;
+  /** Name of the page */
+  name: string;
+  /** Element to be rendered in the page */
+  element: JSX.Element;
+}
+
+/**
+ * Returns a page interface with the given parameters.
+ * @param path Path of the page.
+ * @param name Name of the page.
+ * @param element Element to be rendered in the page.
+ */
+export const page = (path: string, name: string, element: JSX.Element) => {
+  return { path, name, element };
+};
+
+/** Object that contains all the pages and those to be rendered in the navbar. */
+export const pages = [
+  page("/", "Home", <Home />),
+  page("/work-experience", "Work Experience", <WorkExperience />),
+];
