e6e9b8ead16d3a356c2eb7bfe839f51d1186e415
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-12-19 18:06:15 -0600

    Add AboutMe component with styling

---
+++ src/components/molecules/AboutMe/AboutMe.tsx
@@ -0,0 +1,15 @@
+import "./AboutMe.scss";
+import MainInfoCard from "../MainInfoCard/MainInfoCard";
+import SkillsCard from "../SkillsCard/SkillsCard";
+
+/** Component for the AboutMe page */
+const AboutMe = () => {
+  return (
+    <>
+      <MainInfoCard />
+      <SkillsCard />
+    </>
+  );
+};
+
+export default AboutMe;
