c9b3bacb04e9e60335813212ee53f4b18f48b522
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-12-21 14:34:46 -0600

    Update router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
-import "./sass/styles.scss"
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import "./sass/styles.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <Router basename="/">
-      <App />
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
